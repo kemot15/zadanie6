d0ca4ebd334926832efc4628b4c6c94644dbc6b8
Tomek <kemot15@tlen.pl>
2019-07-19 00:58:28 +0200

    zad 2 ver 1

---
+++ src/zadanie6/zad2/Array.java
@@ -0,0 +1,25 @@
+package zadanie6.zad2;
+
+import java.util.Scanner;
+
+public class Array {
+    public void setArray (int[] array){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe 1");
+        array[0] = scanner.nextInt();
+        System.out.println("Podaj liczbe 2");
+        array[1] = scanner.nextInt();
+        System.out.println("Podaj liczbe 3");
+        array[2] = scanner.nextInt();
+        System.out.println("Podaj liczbe 4");
+        array[3] = scanner.nextInt();
+        System.out.println("Podaj liczbe 5");
+        array[4] = scanner.nextInt();
+    }
+
+    public String showArray(int[] array){
+        return "Suma liczb: " + (array[0] + array[2] + array[4]);
+    }
+
+
+}
---
+++ src/zadanie6/zad2/ArrayTest.java
@@ -0,0 +1,10 @@
+package zadanie6.zad2;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        Array array1 =  new Array();
+        array1.setArray(array);
+        System.out.println(array1.showArray(array));
+    }
+}
